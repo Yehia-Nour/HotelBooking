21a3e71b4079b8e89d16252e424bccd0d3b210f1
Yehia <yehia.noureldin.28@gmail.com>
2025-12-13 14:03:51 +0200

    feat: add global exception handling middleware for 404 endpoints

---
+++ src/HotelBooking.Presentation/CustomMiddlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBooking.Presentation.CustomMiddlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next.Invoke(httpContext);
+                await HandelNotFoundEndpointAsync(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something Went Wrong");
+                var problem = new ProblemDetails
+                {
+                    Title = "Error While Processing HTTP Request",
+                    Detail = ex.Message,
+                    Instance = httpContext.Request.Path,
+                    Status = StatusCodes.Status500InternalServerError
+                };
+                httpContext.Response.StatusCode = problem.Status.Value;
+                await httpContext.Response.WriteAsJsonAsync(problem);
+            }
+        }
+
+        private static async Task HandelNotFoundEndpointAsync(HttpContext httpContext)
+        {
+            if (httpContext.Response.StatusCode == StatusCodes.Status404NotFound && !httpContext.Response.HasStarted)
+            {
+                var problem = new ProblemDetails
+                {
+                    Title = "Error While Processing the HTTP Request - Endpiont Not Found",
+                    Detail = $"Endpint {httpContext.Request.Path} Not Found",
+                    Status = StatusCodes.Status404NotFound,
+                    Instance = httpContext.Request.Path
+                };
+                await httpContext.Response.WriteAsJsonAsync(problem);
+            }
+        }
+    }
+}
