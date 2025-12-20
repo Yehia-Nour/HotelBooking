b633621b633d4f4907a21cdc77c0f7ec4fab143b
Yehia <yehia.noureldin.28@gmail.com>
2025-12-20 11:54:56 +0200

    feat: add unit of work

--- src/HotelBooking.Application/Interfaces/IUnitOfWork.cs
+++ src/HotelBooking.Application/Interfaces/IUnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HotelBooking.Application.Interfaces.Repositories;
+using HotelBooking.Domain.Entities.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,7 @@ namespace HotelBooking.Application.Interfaces
 {
     public interface IUnitOfWork
     {
+        IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseEntity;
+        Task<int> SaveChangesAsync();
     }
 }
--- src/HotelBooking.Infrastructure/Implementations/UnitOfWork.cs
+++ src/HotelBooking.Infrastructure/Implementations/UnitOfWork.cs
@@ -1,4 +1,9 @@
 ﻿using HotelBooking.Application.Interfaces;
+using HotelBooking.Application.Interfaces.Repositories;
+using HotelBooking.Domain.Entities.Common;
+using HotelBooking.Infrastructure.Data.DbContexts;
+using HotelBooking.Infrastructure.Implementations.Repositories;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +14,24 @@ namespace HotelBooking.Infrastructure.Implementations
 {
     internal class UnitOfWork : IUnitOfWork
     {
+        private readonly HotelBookingDbContext _context;
+        private readonly Dictionary<Type, object> _reposiotries = [];
+
+        public UnitOfWork(HotelBookingDbContext context) { _context = context; }
+
+        public IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseEntity
+        {
+            var entityType = typeof(TEntity);
+
+            if (_reposiotries.TryGetValue(entityType, out object? reposiotry))
+                return (IGenericRepository<TEntity>)reposiotry;
+
+            var newRepo = new GenericRepository<TEntity>(_context);
+            _reposiotries[entityType] = newRepo;
+
+            return newRepo;
+        }
+
+        public async Task<int> SaveChangesAsync() => await _context.SaveChangesAsync();
     }
 }
